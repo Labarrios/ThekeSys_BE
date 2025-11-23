6a62220fda8a42432c71b5080f5bc012caaa15ca
Israel Labarrios <israel.labarrios.13@gmail.com>
2025-11-22 22:35:38 -0600

    Continue with construction of data map
    - Update the data in the classes of the data folder
    - Add the enum of the porject

--- src/main/kotlin/com/ilr/thekebe/data/Product.kt
+++ src/main/kotlin/com/ilr/thekebe/data/Product.kt
@@ -1,7 +1,19 @@
 package com.ilr.thekebe.data
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
 @Entity
 @Table(name = "Product")
 class Product (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    var productId: Long? = null,
 
+    
 )
